5e7a915c8bc7b495e2d0a23e07baa903e96891b1
Jashema Panapa <jcpanapa@gmail.com>
2020-05-22 13:04:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears VALUES ('Mr. Chocolate', 20, 'M', 0);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Rowdy', 10, 'M', 1);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Tabitha', 6, 'F', 'dark brown', 1);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Sergeant Brown', 3, 'M', 0);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Melissa', 13, 'F', 1);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Grinch', 2, 'M', 'Black', 1);
-INSERT INTO bears (name, age, gender, color, alive) VALUES ('Wendy', 6, 'F', 'Blue', 1);
-INSERT INTO bears (name, age, gender, color, alive) VALUES (NULL, NULL, NULL, NULL);
+INSERT INTO bears VALUES ('Rowdy', 10, 'M', 1);
+INSERT INTO bears VALUES ('Tabitha', 6, 'F', 'dark brown', 1);
+INSERT INTO bears VALUES ('Sergeant Brown', 3, 'M', 0);
+INSERT INTO bears VALUES ('Melissa', 13, 'F', 1);
+INSERT INTO bears VALUES ('Grinch', 2, 'M', 'Black', 1);
+INSERT INTO bears VALUES ('Wendy', 6, 'F', 'Blue', 1);
+INSERT INTO bears VALUES (NULL, NULL, NULL, NULL);
