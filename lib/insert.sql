f2393ee6c7342365079d484efc33234ee713a7d3
Jashema Panapa <jcpanapa@gmail.com>
2020-05-22 12:59:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, alive) VALUES ('Mr. Chocolate', 20, 'M', 0);
 INSERT INTO bears (name, age, gender, color, alive) VALUES ('Rowdy', 10, 'M', 1);
 INSERT INTO bears (name, age, gender, color, alive) VALUES ('Tabitha', 6, 'F', 'dark brown', 1);
-INSERT INTO bears (name, age, gender, alive) VALUES ('Sergeant Brown', 3, 'M', 0);
-INSERT INTO bears (name, age, gender, alive) VALUES ('Melissa', 13, 'F', 1);
-INSERT INTO bears (name, age, gender, alive) VALUES ('Grinch', 2, 'M', 'Black', 1);
+INSERT INTO bears (name, age, gender, color, alive) VALUES ('Sergeant Brown', 3, 'M', 0);
+INSERT INTO bears (name, age, gender, color, alive) VALUES ('Melissa', 13, 'F', 1);
+INSERT INTO bears (name, age, gender, color, alive) VALUES ('Grinch', 2, 'M', 'Black', 1);
 INSERT INTO bears (name, age, gender, alive) VALUES ('Wendy', 6, 'F', 'Blue', 1);
 INSERT INTO bears (name, age, gender, alive) VALUES (NULL, NULL, NULL, NULL);
