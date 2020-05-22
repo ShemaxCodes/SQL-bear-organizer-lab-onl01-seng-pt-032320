83be5b328e851fd08d2baeb50933825dafa13b0d
Jashema Panapa <jcpanapa@gmail.com>
2020-05-22 13:10:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0);
-INSERT INTO bears VALUES ('Rowdy', 10, 'M', 'black', 'intense', 1);
-INSERT INTO bears VALUES ('Tabitha', 6, 'F', 'dark brown', 'nice', 1);
-INSERT INTO bears VALUES ('Sergeant Brown', 3, 'M', 'green', 'silly', 0);
-INSERT INTO bears VALUES ('Melissa', 13, 'F', 'dark brown', 'goofy', 1);
-INSERT INTO bears VALUES ('Grinch', 2, 'M', 'Black', 'mean', 1);
-INSERT INTO bears VALUES ('Wendy', 6, 'F', 'Blue', 'nice', 1);
-INSERT INTO bears VALUES (NULL, 20, 'M', 'black', 'aggressive', 0);
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0);
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'M', 'black', 'intense', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 6, 'F', 'dark brown', 'nice', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 3, 'M', 'green', 'silly', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 13, 'F', 'dark brown', 'goofy', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 2, 'M', 'Black', 'mean', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 6, 'F', 'Blue', 'nice', 1);
+INSERT INTO bears VALUES (8, NULL, 20, 'M', 'black', 'aggressive', 0);
